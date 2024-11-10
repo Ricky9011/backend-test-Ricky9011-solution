f79ad2317d8a517bacb96a7c4ac3e63709dafd2e
BassamMasry <bassam.m.masry@gmail.com>
2024-11-10 19:25:27 +0200

    removed log function (celery task will log), improved error messages

--- src/users/use_cases/create_user.py
+++ src/users/use_cases/create_user.py
@@ -1,9 +1,9 @@
 from typing import Any
 
 import structlog
+from django.db.utils import IntegrityError
 
 from core.base_model import Model
-from core.event_log_client import EventLogClient
 from core.use_case import UseCase, UseCaseRequest, UseCaseResponse
 from users.models import User
 
@@ -38,30 +38,17 @@ class CreateUser(UseCase):
     def _execute(self, request: CreateUserRequest) -> CreateUserResponse:
         logger.info('creating a new user')
 
-        user, created = User.objects.get_or_create(
-            email=request.email,
-            defaults={
-                'first_name': request.first_name, 'last_name': request.last_name,
-            },
-        )
-
-        if created:
+        try:
+            user = User.objects.create(
+                email=request.email,
+                first_name=request.first_name,
+                last_name=request.last_name,
+            )
             logger.info('user has been created')
-            self._log(user)
             return CreateUserResponse(result=user)
+        except IntegrityError:
+            logger.error('unable to create a new user as it already exists')
+            return CreateUserResponse(error='User with this email already exists')
 
         logger.error('unable to create a new user')
-        return CreateUserResponse(error='User with this email already exists')
-
-    def _log(self, user: User) -> None:
-        with EventLogClient.init() as client:
-            client.insert(
-                data=[
-                    UserCreated(
-                        email=user.email,
-                        first_name=user.first_name,
-                        last_name=user.last_name,
-                    ),
-                ],
-            )
-
+        return CreateUserResponse(error='Unknown error while creating a new user')
