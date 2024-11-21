060914cc85369a25340373b019e4ce3f0d537763
i.pakeev <i.pakeev.ktsstudio@gmail.com>
2024-11-21 15:16:54 +0300

    revert transaction logic

--- src/logs/outbox.py
+++ src/logs/outbox.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 
 import structlog
 from django.conf import settings
+from django.db import transaction
 from django.utils import timezone
 from pydantic import BaseModel
 
@@ -40,35 +41,30 @@ class OutboxExporter:
         Экспортирует не экспортированные ранее логи из базы данных в ClickHouse.
         Экспорт выполняется батчами.
         После экспорта помечает логи как экспортированные.
-
-        В docker compose запущен лишь 1 селери воркер с concurrency=1, поэтому все таски
-        выполняются последовательно, и нет необходимости использовать транзакцию
-        для блокировки записей.
-
-        При добавлении новых тасок имеет смысл также оставить 1 воркер с concurrency=1,
-        который смотрит только на очередь outbox.
-        Для остальных очередей создать свои воркеры с любым concurrency.
         """
         batch_size = batch_size or settings.CLICKHOUSE_BATCH_SIZE
 
         while True:
-            qs = OutboxLog.objects.filter(exported_at__isnull=True).order_by("id")
-            objs = qs[:batch_size]
-            if not objs:
-                logger.info("no data to export, exiting")
-                return
+            with transaction.atomic():
+                objs = (
+                    OutboxLog.objects.filter(exported_at__isnull=True)
+                    .select_for_update()
+                    .order_by("id")[:batch_size]
+                )
+                if not objs:
+                    logger.info("no data to export, exiting")
+                    return
 
-            data = cls._convert_data(objs)
+                data = cls._convert_data(objs)
+                OutboxLog.objects.filter(id__in=[obj.id for obj in objs]).update(
+                    exported_at=timezone.now(),
+                )
 
-            client: ClickHouseClient
-            with ClickHouseClient.init() as client:
-                client.insert(data)
+                client: ClickHouseClient
+                with ClickHouseClient.init() as client:
+                    client.insert(data)
 
-            logger.info("inserted data to clickhouse", count=len(data))
-
-            OutboxLog.objects.filter(id__in=[obj.id for obj in objs]).update(
-                exported_at=timezone.now(),
-            )
+                logger.info("inserted data to clickhouse", count=len(data))
 
     @staticmethod
     def _convert_data(
