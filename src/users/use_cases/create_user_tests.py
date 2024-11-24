fa514e6b504df2384be262410494ad9f7618f131
Elshan <91986720+EshKG@users.noreply.github.com>
2024-11-24 17:22:46 +0300

    upload edited create_user and tests

--- src/users/use_cases/create_user_tests.py
+++ src/users/use_cases/create_user_tests.py
@@ -1,12 +1,14 @@
+import json
 import uuid
 from collections.abc import Generator
-from unittest.mock import ANY
 
 import pytest
 from clickhouse_connect.driver import Client
 from django.conf import settings
 
-from users.use_cases import CreateUser, CreateUserRequest, UserCreated
+from outbox_pattern.models import EventOutbox
+from outbox_pattern.tasks import process_outbox_events
+from users.use_cases import CreateUser, CreateUserRequest
 
 pytestmark = [pytest.mark.django_db]
 
@@ -49,20 +51,50 @@ def test_event_log_entry_published(
     f_use_case: CreateUser,
     f_ch_client: Client,
 ) -> None:
-    email = f'test_{uuid.uuid4()}@email.com'
+    email = f"test_{uuid.uuid4()}@email.com"
     request = CreateUserRequest(
         email=email, first_name='Test', last_name='Testovich',
     )
 
     f_use_case.execute(request)
-    log = f_ch_client.query("SELECT * FROM default.event_log WHERE event_type = 'user_created'")
 
-    assert log.result_rows == [
-        (
-            'user_created',
-            ANY,
-            'Local',
-            UserCreated(email=email, first_name='Test', last_name='Testovich').model_dump_json(),
-            1,
-        ),
-    ]
+    # Trigger the process to handle events from EventBox and insert them into ClickHouse
+    process_outbox_events()
+
+    # Query ClickHouse to retrieve events with the 'user has been created' event type
+    log = f_ch_client.query(
+        "SELECT * FROM default.event_log WHERE event_type = 'user has been created'",
+    )
+
+    # Assert that exactly one event was found in ClickHouse
+    assert len(log.result_rows) == 1
+
+    selected_row = log.result_rows[0]
+    selected_row_context = json.loads(selected_row[3])
+
+    right_event_context = {
+        "email": email,
+        "first_name": "Test",
+        "last_name": "Testovich",
+    }
+
+    # Verify that the context of the log matches the expected event data
+    assert selected_row_context == right_event_context
+
+
+def test_event_created_outbox(f_use_case: CreateUser) -> None:
+    email = "test@example.com"
+    request = CreateUserRequest(
+        email=email,
+        first_name="Test",
+        last_name="Testovich",
+    )
+
+    f_use_case.execute(request)
+
+    # Check that the event has been created in the EventOutbox
+    event = EventOutbox.objects.get(event_context__email=email)
+
+    # Verify that the event type is correctly set to "user has been created"
+    assert event.event_type == "user has been created"
+
